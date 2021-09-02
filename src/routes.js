e38d0227482e2c11211c0ab99c6e7af016b1f5c7
igor henrique <igorhlr@hotmail.com>
2021-09-01 21:30:24 -0300

    criando e passando objeto para profile.ejs

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,20 @@ const routes = express.Router()
 
 const views = __dirname + "/views/"
 
+const profile = {
+  name: "Igor",
+  avatar: "https://avatars.githubusercontent.com/u/69877510?v=4",
+  "mounthly-budget": 10000,
+  "days-per-week": 5,
+  "hours-per-day": 5,
+  "vacation-per-year": 4
+}
+
 //request, response
 routes.get('/', (req, res) => res.render(views + "index"))
 routes.get('/job', (req, res) => res.render(views + "job")) 
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
-routes.get('/profile', (req, res) => res.render(views + "profile")) 
+routes.get('/profile', (req, res) => res.render(views + "profile", { profile })) 
 
 
 
